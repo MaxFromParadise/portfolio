13db8bc18e33b437594f88d7a913260685b072fa
Maxim <maxdemented372@gmail.com>
2022-11-05 18:05:17 +0600

    desctopReady

--- src/js/script.js
+++ src/js/script.js
@@ -8,4 +8,49 @@ hamburger.addEventListener("click", () => {
 closeMenu.addEventListener("click", () => {
     navbar.classList.remove("active")
 });
+const counters = document.querySelectorAll(".level__item-header_percentage"),
+      lines = document.querySelectorAll(".level__item-progress_full");
+counters.forEach((item, i) => {
+    lines[i].style.width = item.innerHTML;
+});
 
+
+$(document).ready(function(){
+    function modalValidate(classValid){
+        $(classValid).validate({
+            rules: {
+                name: "required",
+                email: {
+                    required: true,
+                    email: true
+                },
+                text: "required",
+                onfocusout: true
+            },
+            messages: {
+                name: "Введите своё имя",
+                email: {
+                  required: "После с email должно быть заполнено!",
+                  email: "Ваш email должен быть в формате name@domain.com!"
+                },
+                text: "Введите сообщение"
+            }
+        });
+    };
+    modalValidate("#contacts-form");
+    $("form").submit(function(e){
+        e.preventDefault();
+        if (!$(this).valid()) {
+            return;
+        }
+        $.ajax({
+            type: "POST",
+            url: "mailer/smart.php",
+            data: $(this).serialize()
+        }).done(function() {
+            $(this).find("input").val("");
+            $("form").trigger("reset");
+            return false;
+        });
+    });
+});
